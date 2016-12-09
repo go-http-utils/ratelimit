90890351a35ce295b66a3aafb6a3c3d76911d21a
DavidCai <davidcai1993@yahoo.com>
2016-12-09 11:08:23 +0800

    support expire

--- ratelimit.go
+++ ratelimit.go
@@ -3,6 +3,7 @@ package ratelimit
 import (
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 
 	bucket "github.com/DavidCai1993/token-bucket"
@@ -10,36 +11,68 @@ import (
 )
 
 // Version is this package's version number.
-const Version = "0.1.0"
+const Version = "0.2.0"
 
 // GetIDFunc represents a function that return an ID for each request.
 // All requests which have the same ID will be regarded from one source and
 // be ratelimited.
 type GetIDFunc func(*http.Request) string
 
+type expireBucket struct {
+	bucket  *bucket.TokenBucket
+	start   time.Time
+	expired time.Time
+}
+
 // Handler wraps the http.Handler with reatelimit support (only count requests
 // can pass through in duration).
 func Handler(h http.Handler, getID GetIDFunc, duration time.Duration, count int64) http.Handler {
-	bucketsMap := map[string]*bucket.TokenBucket{}
+	mutex := sync.Mutex{}
+	bucketsMap := map[string]*expireBucket{}
 	interval := count / int64(duration/1e9) * 1e9
+
+	go func() {
+		for now := range time.Tick(duration) {
+			mutex.Lock()
+
+			for id, eb := range bucketsMap {
+				if eb.expired.After(now) {
+					delete(bucketsMap, id)
+					eb.bucket.Destory()
+				}
+			}
+
+			mutex.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		id := getID(req)
 
+		mutex.Lock()
+
 		b, ok := bucketsMap[id]
 
 		if !ok {
-			b = bucket.New(time.Duration(interval), count)
+			b = &expireBucket{
+				bucket:  bucket.New(time.Duration(interval), count),
+				expired: time.Now().Add(duration * 5),
+			}
 
 			bucketsMap[id] = b
 		}
 
-		ok = b.TryTake(1)
-		avail := b.Availible()
+		ok = b.bucket.TryTake(1)
+		avail := b.bucket.Availible()
+		cap := b.bucket.Capability()
+		b.expired = time.Now().Add(duration * 5)
+
+		mutex.Unlock()
+
 		resHeader := res.Header()
 
 		if ok {
-			resHeader.Set(headers.XRatelimitLimit, strconv.FormatInt(b.Capability(), 10))
+			resHeader.Set(headers.XRatelimitLimit, strconv.FormatInt(cap, 10))
 			resHeader.Set(headers.XRatelimitRemaining, strconv.FormatInt(avail, 10))
 
 			h.ServeHTTP(res, req)
